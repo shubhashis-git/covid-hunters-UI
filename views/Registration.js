8b829fddf9f92e551fbc4e5ec20437091de686de
Ankan Roy <ankan.roy@cognizant.com>
2020-04-16 14:14:04 +0000

    Registration progress

--- views/Registration.js
+++ views/Registration.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
 
 class Registration extends Component {
   constructor(props) {
@@ -9,7 +9,24 @@ class Registration extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Registration Page</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.inputBox} 
+            placeholder="Please enter your Name."
+          />
+          <TextInput
+            style={styles.inputBox} 
+            placeholder="Please enter your mobile number."
+          />
+          <TextInput
+            style={styles.inputBox} 
+            placeholder="Please enter your address."
+          />
+          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+            <Button title="Register Here" onPress={() => this.props.navigation.navigate('Registration')} color="red" />
+            <Button title="Login Here" onPress={() => this.props.navigation.navigate('Login')} color="orange" />
+          </View>
+        </View>
       </View>
     );
   }
@@ -18,7 +35,18 @@ class Registration extends Component {
 const styles = StyleSheet.create({
   container: {
     margin: 30
-  }
+  },
+  inputContainer: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  inputBox: {   
+    borderColor: '#CCC',
+    borderWidth: 2,
+    margin: 10,
+    padding: 10
+  },
 });
 
 export default Registration;
